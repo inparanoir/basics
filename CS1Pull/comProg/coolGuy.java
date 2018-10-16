1d9dbfb4bd859a5130b4142a5db54e3d8c3266d2
inparanoir <35826569+inparanoir@users.noreply.github.com>
2018-10-16 13:32:36 -0500

    Create coolGuy.java

---
+++ CS1Pull/comProg/coolGuy.java
@@ -0,0 +1,19 @@
+package competitiveProgramming;
+
+public class test
+{
+	public static void main( String [] args )
+	{
+		String coolGuy = "adfsfsdfd";
+		coolGuy += 'C';
+		System.out.println(coolGuy);
+
+		if ( coolGuy.equals(coolGuy) )
+		{
+			System.out.println();
+			System.out.println("Yay!!!");
+		}
+
+	}
+
+}
